2ef72139508852ec6aab5c7fc5379c0721e76a6b
Gui-dev <adrianon10@gmail.com>
2022-09-03 02:45:30 -0300

    feat: add Interface scene

---
+++ scripts/management/interface.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+class_name Interface
+
+
+onready var current_weapon: Label = $current_weapon
+onready var ammo: Label = $ammo
+
+
+func set_current_weapon(weapon: String) -> void:
+  current_weapon.text = 'Current Weapon: ' + weapon
+
+
+func set_ammo(current_ammo: int, max_ammo: int) -> void:
+  ammo.text = 'Ammo: ' + str(current_ammo) + '/' + str(max_ammo)
