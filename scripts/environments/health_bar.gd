e1d1aad8d08ee457035e79967269067b097e0e1e
Gui-dev <adrianon10@gmail.com>
2022-09-06 03:25:41 -0300

    feat: add activating life bar when hitting

---
+++ scripts/environments/health_bar.gd
@@ -0,0 +1,29 @@
+extends TextureProgress
+class_name HealthBar
+
+
+onready var tween: Tween = $tween
+var current_health: int
+
+
+func init_bar(bar_max_value: int) -> void:
+  max_value = bar_max_value
+  value = bar_max_value
+  current_health = bar_max_value
+
+
+func update_value(new_value: int) -> void:
+  update_bar(current_health, new_value)
+  current_health = new_value
+  
+
+func update_bar(old_value: int, new_value: int) -> void:
+  var _interpolate_value: bool = tween.interpolate_property(
+    self,
+    'value',
+    old_value,
+    new_value,
+    .2  
+  )  
+  
+  var _start = tween.start()
