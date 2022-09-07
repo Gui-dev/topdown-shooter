59460e39abd8799da11aee5fd52aa812f4d83fe6
Gui-dev <adrianon10@gmail.com>
2022-09-07 16:09:53 -0300

    feat: add helper function reaload_game

--- scripts/management/interface.gd
+++ scripts/management/interface.gd
@@ -12,3 +12,7 @@ func set_current_weapon(weapon: String) -> void:
 
 func set_ammo(current_ammo: int, max_ammo: int) -> void:
   ammo.text = 'Ammo: ' + str(current_ammo) + '/' + str(max_ammo)
+  
+
+func reload_game() -> void:
+  var _reload = get_tree().change_scene("res://scenes/management/game_level.tscn")
