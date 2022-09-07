3e509cac28b8a0c0eb3c402134998f9fd7340d11
Gui-dev <adrianon10@gmail.com>
2022-09-07 20:01:52 -0300

    feat: add spot light

--- scripts/player/soldier_texture.gd
+++ scripts/player/soldier_texture.gd
@@ -65,5 +65,4 @@ func _on_animation_finished(anim_name: String) -> void:
     'hit':
       parent.set_physics_process(true)
     'death':
-      yield(get_tree().create_timer(1), 'timeout')
       get_tree().call_group('interface', 'reload_game')
